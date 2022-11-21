b50e860bb88937744fecc63b882e50f5b7e3c58a
kalam <kalam@Sasi>
2022-11-21 12:50:44 +0530

    Framework

---
+++ src/test/java/MVN_Practice_package/SampleMavenTest1.java
@@ -0,0 +1,19 @@
+package MVN_Practice_package;
+
+import org.testng.annotations.Test;
+
+public class SampleMavenTest1 {
+	public class Sample1Test {
+		@Test
+		public void tc1() {
+			System.out.println("Hi");
+			
+		}
+		@Test
+		public void tc1_1() {
+			System.out.println("hi");
+			
+		}
+
+}
+}
